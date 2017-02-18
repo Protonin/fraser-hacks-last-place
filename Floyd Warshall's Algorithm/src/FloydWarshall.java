9d71d6dba25090e55afe29c70e5ca07dc8beb47a
Tiger Kong <starlazer.lightspeed@gmail.com>
2017-02-18 17:14:52 -0500

    Add files via upload

--- Floyd Warshall's Algorithm/src/FloydWarshall.java
+++ Floyd Warshall's Algorithm/src/FloydWarshall.java
@@ -3,34 +3,29 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class FloydWarshall {
-	static Vector<Vector<Double> > a = new Vector<Vector<Double> >();
-	static Vector<Integer> vert = new Vector<Integer> ();
-	static Vector<Double> temp = new Vector<Double>();
-	static Vector<Integer> temp1 = new Vector<Integer>();
-	static Vector<Vector<Integer> > paths = new Vector<Vector<Integer> >();
-	static int n = 4;
+	static int n = 5;
+	static double[][] a = new double[n][n];
+	static int[] vert = new int[n];
+	static int[][] paths = new int[n][n];
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
+		for (int x = 0; x < n; x++)
+		{
+			for (int y = 0; y < n; y++)
+			{
+				a[x][y] = 9999;
+				paths[x][y] = n;
+			}
+		}
+		for (int x = 0; x < n; x++)
+		{
+			paths[x][x] = n;
+		}
 		int thing1 = 99;
 		int thing2 = 0;
 		double thing3 = 0;
-		for (int x = 0; x < n/* mod later */; x++)
-		{
-			temp.add(0.0);
-			temp1.add(0);
-		}
-		for (int x = 0; x < n; x++)
-		{
-			a.add(temp);
-			paths.add(temp1);
-		}
-		for (int x = 0; x < n; x++)
-		{
-			vert.add(x);
-			paths.get(x).set(x, 101);
-			
-		}
 		while (thing1 != thing2)
 		{
 			thing1 = in.nextInt();
@@ -40,7 +35,8 @@ public class FloydWarshall {
 				break;
 			}
 			thing3 = in.nextDouble();
-			a.get(thing1).set(thing2, thing3);
+			a[thing1][thing2] = thing3;
+			paths[thing1][thing2] = thing1;
 		}
 		System.out.println("Input done.");
 		// Here lieth the api import code
@@ -48,29 +44,78 @@ public class FloydWarshall {
 		{
 			for (int y = 0; y < n; y++)
 			{
-				if (x != y)
-				{
-					paths.get(x).set(y, x);
-				}
+				System.out.print(paths[x][y]);
+				System.out.print(" ");
 			}
+			System.out.println();
 		}
+		System.out.println();
 		for (int x = 0; x < n; x++)
 		{
 			for (int y = 0; y < n; y++)
 			{
+				if (y == x)
+				{
+					continue;
+				}
 				for (int z = 0; z < n; z++)
 				{
-					if (a.get(y).get(z)> a.get(x).get(z)+a.get(y).get(x))
+					if (z == y || z == x || a[x][z] == 9999 || a[y][x] == 9999)
 					{
-						a.get(y).set(z, a.get(x).get(z)+a.get(y).get(x));
-						paths.get(y).set(z, x);
+						continue;
+					}
+					if (a[y][z] > a[y][x] + a[x][z])
+					{
+						a[y][z] = a[y][x] + a[x][z];
+						paths[y][z] = paths[x][z];
+						System.out.print(y);
+						System.out.print(" ");
+						System.out.print(z);
+						System.out.print(" ");
+						System.out.print(x);
+						System.out.println();
 					}	
 				}
 			}
 		}
+		System.out.println();
+		for (int x = 0; x < n; x++)
+		{
+			for (int y = 0; y < n; y++)
+			{
+				System.out.print(paths[x][y]);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for (int x = 0; x < n; x++)
+		{
+			for (int y = 0; y < n; y++)
+			{
+				System.out.print(a[x][y]);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 		Scanner sc = new Scanner(System.in);
 		int node = sc.nextInt();
-		System.out.println(a.get(0).get(node));
-		
+		int node1 = sc.nextInt();
+		System.out.println(node1);
+		tracePath(node, node1);
+		System.out.println(a[node][node1]);
+	}
+	
+	public static void tracePath(int start, int stop)
+	{
+		if (paths[start][stop] == start)
+		{
+			System.out.println(start);
+		}
+		else
+		{
+			System.out.println(paths[start][stop]);
+			tracePath(start, paths[start][stop]);
+		}
 	}
 }
